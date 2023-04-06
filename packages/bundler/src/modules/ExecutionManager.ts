7bdddfa26894dc6c432a119a3192f7a57e4df328
PPYang <PPYang@gopluslabs.io>
2023-04-06 20:35:20 +0800

    remove log

--- packages/bundler/src/modules/ExecutionManager.ts
+++ packages/bundler/src/modules/ExecutionManager.ts
@@ -80,7 +80,7 @@ export class ExecutionManager {
    * @param force
    */
   async attemptBundle (force = true): Promise<SendBundleReturn | undefined> {
-    debug('attemptBundle force=', force, 'count=', this.mempoolManager.count(), 'max=', this.maxMempoolSize)
+    // debug('attemptBundle force=', force, 'count=', this.mempoolManager.count(), 'max=', this.maxMempoolSize)
     if (force || this.mempoolManager.count() >= this.maxMempoolSize) {
       const ret = await this.bundleManager.sendNextBundle()
       if (this.maxMempoolSize === 0) {
